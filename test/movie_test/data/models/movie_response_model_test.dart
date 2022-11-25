a8ec719fd824e27bfb40f9a400f996de5c57c1f3
Andik-phur <Naduiki@gmail.com>
2022-11-25 15:09:38 +0700

    third

--- test/movie_test/data/models/movie_response_model_test.dart
+++ test/movie_test/data/models/movie_response_model_test.dart
@@ -29,7 +29,7 @@ void main() {
     test('should return a valid model from JSON', () async {
       // arrange
       final Map<String, dynamic> jsonMap =
-          json.decode(readJson('dummy_data/now_playing.json'));
+          json.decode(readJson('movie_test/dummy_data/now_playing.json'));
       // act
       final result = MovieResponse.fromJson(jsonMap);
       // assert
