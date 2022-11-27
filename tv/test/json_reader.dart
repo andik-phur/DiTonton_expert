54045d6718affa7c0d9bf378fff926a297ec00ad
Andik-phur <Naduiki@gmail.com>
2022-11-27 13:19:57 +0700

    json reader fix

--- tv/test/json_reader.dart
+++ tv/test/json_reader.dart
@@ -5,8 +5,8 @@ String readJson(String name) {
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  if (dir.endsWith('movie')) {
+  if (dir.endsWith('tv')) {
     return File('$dir/test/$name').readAsStringSync();
   }
-  return File('$dir/movie/test/$name').readAsStringSync();
+  return File('$dir/tv/test/$name').readAsStringSync();
 }
